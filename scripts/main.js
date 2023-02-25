3b73d75241d74fe12d0807ef20f3a8a6bd1642cb
Simar Ubhi <73083235+simarubhi@users.noreply.github.com>
2023-02-25 12:58:47 -0600

    git commit -m "plan created"

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const backGroundContainer = document.querySelector('.background-container');
+const spaceContainer = document.querySelector('.space-container');
+const nonFireRocket = document.querySelector('.rocket.no-fire');
+const fireRocket = document.querySelector('.rocket.fire');
+
+const altText = document.querySelector('.stat-text.alt');
+const velText = document.querySelector('.stat-text.vel');
+const fuelText = document.querySelector('.stat-text.fuel');
